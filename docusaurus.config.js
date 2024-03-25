beda8dfc5a063746697112fb88d7dc178d0466a0
May <112294121+ave9858@users.noreply.github.com>
2024-03-25 21:47:33 +0000

    Update docusaurus.config.js
    disable blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,13 +47,7 @@ const config = {
           editUrl:
             'https://github.com/gravesoft/gravesoft-site/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/gravesoft/gravesoft-site/tree/main/',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
