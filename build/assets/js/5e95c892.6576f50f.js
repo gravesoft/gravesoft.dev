62ff6fedbb515bc5b771f2d077bf33c65fde9051
WindowsAddict <windowsaddict@protonmail.com>
2024-05-15 09:27:50 +0530

    Delete build folder

--- build/assets/js/5e95c892.6576f50f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgravesoft=self.webpackChunkgravesoft||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});r(6540);var a=r(4164),t=r(1003),u=r(7559),c=r(2831),n=r(781),f=r(4848);function l(e){return(0,f.jsx)(t.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,f.jsx)(n.A,{children:(0,c.v)(e.route.routes)})})}}}]);
